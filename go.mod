767c0a9510cb11ddd64252292ad1146cab7f7251
Denis Issoupov <dissoupov@salesforce.com>
2020-06-26 10:37:11 -0700

    embed templates

--- go.mod
+++ go.mod
@@ -4,11 +4,11 @@ go 1.14
 
 require (
 	github.com/go-phorce/cov-report v1.1.1-0.20200622030546-3fb510c4b1ba
-	github.com/jteeuwen/go-bindata v3.0.7+incompatible
 	github.com/juju/errors v0.0.0-20200330140219-3fe23663418f
 	github.com/juju/testing v0.0.0-20200608005635-e4eedbc6f7aa // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/mattn/goveralls v0.0.6
+	github.com/omeid/go-resources v0.0.0-20200113210624-eb442c910d63
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.6.1
 	golang.org/x/lint v0.0.0-20200302205851-738671d3881b
---
+++ vendor/github.com/omeid/go-resources/cmd/resources/cmd.go
@@ -0,0 +1,84 @@
+// Unfancy resources embedding with Go.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"path/filepath"
+	"time"
+
+	"github.com/omeid/go-resources"
+)
+
+var (
+	pkg      = "main"
+	varName  = "FS"
+	tag      = ""
+	declare  = false
+	out      = ""
+	trimPath = ""
+	width    = resources.BlockWidth
+	gofmt    = false
+)
+
+type nope struct{}
+
+func main() {
+	flag.StringVar(&pkg, "package", pkg, "`name` of the package to generate")
+	flag.StringVar(&varName, "var", varName, "`name` of the variable to assign the virtual filesystem to")
+	flag.StringVar(&tag, "tag", tag, "`tag` to use for the generated package (default no tag)")
+	flag.BoolVar(&declare, "declare", declare, "whether to declare the -var (default false)")
+	flag.StringVar(&out, "output", out, "`filename` to write the output to")
+	flag.StringVar(&trimPath, "trim", trimPath, "path `prefix` to remove from the resulting file path in the virtual filesystem")
+	flag.IntVar(&width, "width", width, "`number` of content bytes per line in generetated file")
+	flag.BoolVar(&gofmt, "fmt", gofmt, "run output through gofmt, this is slow for huge files (default false)")
+	flag.Parse()
+
+	if out == "" {
+		flag.PrintDefaults()
+		log.Fatal("-output is required.")
+	}
+
+	config := resources.Config{
+		Pkg:     pkg,
+		Var:     varName,
+		Tag:     tag,
+		Declare: declare,
+		Format:  gofmt,
+	}
+	resources.BlockWidth = width
+
+	res := resources.New()
+	res.Config = config
+
+	files := make(map[string]nope)
+
+	for _, g := range flag.Args() {
+		matches, err := filepath.Glob(g)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, m := range matches {
+			files[m] = nope{}
+		}
+	}
+
+	t0 := time.Now()
+
+	var wrote int
+	for file := range files {
+		n, err := res.AddFiles(trimPath, file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		wrote += n
+	}
+
+	if err := res.Write(out); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Finished in %v. Wrote %d resources to %s", time.Since(t0), wrote, out)
+}
